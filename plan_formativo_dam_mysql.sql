187b2cef40ba7b29dbdcfcc8c4270e2ea2148a69
Gonzalo Pérez Cabrera <148452834+GonzaloPZC@users.noreply.github.com>
2025-03-07 10:50:28 +0000

    Add files via upload

---
+++ plan_formativo_dam_mysql.sql
@@ -0,0 +1,18 @@
+CREATE TABLE plan_formativo_dam (
+    `B` TEXT,
+    `Diseño_y_manejo_de_bases_de_datos` TEXT,
+    `nan` TEXT,
+    `nan` TEXT,
+    `nan` TEXT,
+    `-_Diseño_lógico_de_bases_de_datos.` TEXT,
+    `nan` TEXT,
+    `nan` TEXT,
+    `nan` TEXT,
+    `nan` TEXT,
+    `nan` TEXT,
+    `Creación_de_un_sistema_de_gestión_de_inventarios_con_MySQL` TEXT
+);
+
+INSERT INTO plan_formativo_dam VALUES ('C', 'Desarrollo y despliegue de juegos', '', '', '', '- Desarrollo de juegos y aplicaciones de entretenimiento.', '', '', '', '', '', 'Creación de un juego de preguntas y respuestas');
+INSERT INTO plan_formativo_dam VALUES ('D', 'Desarrollo y pruebas de interfaz de aplicaciones multiplatafoma', '', '', '', '- Aplicaciones en red.', '', '', '', '', '', 'Desarrollo de una interfaz responsiva para una app de gestión de tareas');
+INSERT INTO plan_formativo_dam VALUES ('E', 'Implantación de sistemas de gestión de recursos empresariales', '', '', '', '- Gestión de la información', '', '', '', '', '', 'Instalación y configuración de Odoo como ERP en una empresa');
