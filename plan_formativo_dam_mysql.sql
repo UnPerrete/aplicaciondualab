d19384abb082bdfdc0d69899c92b8c6ac66ef2f2
Gonzalo Pérez Cabrera <148452834+GonzaloPZC@users.noreply.github.com>
2025-03-07 10:55:37 +0000

    Update plan_formativo_dam_mysql.sql

--- plan_formativo_dam_mysql.sql
+++ plan_formativo_dam_mysql.sql
@@ -1,18 +1,12 @@
-CREATE TABLE plan_formativo_dam (
-    `B` TEXT,
-    `Diseño_y_manejo_de_bases_de_datos` TEXT,
-    `nan` TEXT,
-    `nan` TEXT,
-    `nan` TEXT,
-    `-_Diseño_lógico_de_bases_de_datos.` TEXT,
-    `nan` TEXT,
-    `nan` TEXT,
-    `nan` TEXT,
-    `nan` TEXT,
-    `nan` TEXT,
-    `Creación_de_un_sistema_de_gestión_de_inventarios_con_MySQL` TEXT
+CREATE TABLE desarrollo_aplicaciones_multiplataforma (
+    Bloque CHAR(1) PRIMARY KEY,
+    Descripcion TEXT,
+    Tareas_A_Realizar TEXT,
+    Ejemplos TEXT
 );
 
-INSERT INTO plan_formativo_dam VALUES ('C', 'Desarrollo y despliegue de juegos', '', '', '', '- Desarrollo de juegos y aplicaciones de entretenimiento.', '', '', '', '', '', 'Creación de un juego de preguntas y respuestas');
-INSERT INTO plan_formativo_dam VALUES ('D', 'Desarrollo y pruebas de interfaz de aplicaciones multiplatafoma', '', '', '', '- Aplicaciones en red.', '', '', '', '', '', 'Desarrollo de una interfaz responsiva para una app de gestión de tareas');
-INSERT INTO plan_formativo_dam VALUES ('E', 'Implantación de sistemas de gestión de recursos empresariales', '', '', '', '- Gestión de la información', '', '', '', '', '', 'Instalación y configuración de Odoo como ERP en una empresa');
+INSERT INTO desarrollo_aplicaciones_multiplataforma VALUES ('A', 'Gestión de equipos y sistemas', 'Opera entornos de desarrollo.', 'Instalación y configuración de sistemas operativos para desarrollo');
+INSERT INTO desarrollo_aplicaciones_multiplataforma VALUES ('B', 'Diseño y manejo de bases de datos', 'Diseño lógico de bases de datos.', 'Diseño de una base de datos para una aplicación de gestión de clientes');
+INSERT INTO desarrollo_aplicaciones_multiplataforma VALUES ('C', 'Desarrollo y despliegue de juegos', 'Desarrollo de juegos y aplicaciones de entretenimiento.', 'Creación de un juego de preguntas y respuestas');
+INSERT INTO desarrollo_aplicaciones_multiplataforma VALUES ('D', 'Desarrollo y pruebas de interfaz de aplicaciones multiplataforma', 'Aplicaciones en red.', 'Desarrollo de una interfaz responsiva para una app de gestión de tareas');
+INSERT INTO desarrollo_aplicaciones_multiplataforma VALUES ('E', 'Implantación de sistemas de gestión de recursos empresariales', 'Gestión de la información', 'Instalación y configuración de Odoo como ERP en una empresa');
