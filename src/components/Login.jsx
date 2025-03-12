f55e5583be3e0caa8491aa188bbd4fd788adb31e
UnPerrete <pelusapower52@gmail.com>
2025-03-12 11:40:13 +0000

    Alternar visibilidad de la contraseña en el Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,25 +3,27 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from '../context/AuthProvider';
 import CryptoJS from "crypto-js";
 import { Link } from "react-router-dom";
-import "../styles/Login.css"
+import "../styles/Login.css";
+import eyeIcon from "../assets/eye.svg";  // Ruta de la imagen del ojo abierto
+import eyeClosed from "../assets/eye-closed.svg"; // Ruta de la imagen del ojo cerrado
 
 const Login = () => {
   const [role, setRole] = useState("profesor");
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
+  const [showPassword, setShowPassword] = useState(false); // Estado para la visibilidad de la contraseña
   const { login } = useAuth();
   const navigate = useNavigate();
 
   // Maneja cambios en los campos del formulario
   const handleChange = (e) => {
     let value = e.target.value;
-        if (e.target.type === "password"){
-          const hashedPass = CryptoJS.MD5(e.target.value).toString(CryptoJS.enc.Hex);
-          value = hashedPass
-        }
-        setFormData({ ...formData, [e.target.name]: value })
+    if (e.target.name === "password") {
+      const hashedPass = CryptoJS.MD5(e.target.value).toString(CryptoJS.enc.Hex);
+      value = hashedPass;
+    }
+    setFormData({ ...formData, [e.target.name]: value });
   };
-
 
   // Enviar formulario
   const handleSubmit = async (e) => {
@@ -60,17 +62,29 @@ const Login = () => {
           <option value="empresa">Empresa</option>
         </select>
 
-          <>
-            <input type="text" name="nif" placeholder="NIF" onChange={handleChange} required />
-            <input type="password" name="password" placeholder="Contraseña" onChange={handleChange} required />
-          </>
-        
-        
+        <input type="text" name="nif" placeholder="NIF" onChange={handleChange} required />
+
+        <div className="password-container">
+          <input
+            type={showPassword ? "text" : "password"}
+            name="password"
+            placeholder="Contraseña"
+            onChange={handleChange}
+            required
+          />
+          <img
+            src={showPassword ? eyeIcon : eyeClosed}
+            alt="Mostrar contraseña"
+            className="eye-icon"
+            onClick={() => setShowPassword(!showPassword)}
+          />
+        </div>
+
         <button type="submit">Iniciar sesión</button>
-        {error && <p style={{ color: "red" }}>{error}</p>}
+        {error && <p>{error}</p>}
       </form>
       <p>
-         ¿No tienes cuenta? <Link to="/register">Regístrate aquí</Link>
+        ¿No tienes cuenta? <Link to="/register">Regístrate aquí</Link>
       </p>
     </div>
   );
