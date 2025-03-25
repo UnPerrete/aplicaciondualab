0e284900fae42e7abd839de9ca8784db2f7c2543
Gonzalo <gonzaloalbertoperezcabrera@alumno.ieselrincon.es>
2025-03-25 13:08:11 +0000

    barra busqueda navbar

--- src/components/NavbarWeb.jsx
+++ src/components/NavbarWeb.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/logo.png'; 
+import logo from '../assets/logo.png';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import '../styles/NavbarWeb.css';
 
@@ -8,27 +8,31 @@ const NavbarWeb = () => {
     const [dropdownOpen, setDropdownOpen] = useState(false);
     const [scrollingUp, setScrollingUp] = useState(true);
     const [lastScrollY, setLastScrollY] = useState(0);
+    const [searchOpen, setSearchOpen] = useState(false);
+    const [searchQuery, setSearchQuery] = useState("");
 
     useEffect(() => {
         const handleScroll = () => {
-          setScrollingUp(window.scrollY < lastScrollY)
-          setLastScrollY(window.scrollY);
+            setScrollingUp(window.scrollY < lastScrollY)
+            setLastScrollY(window.scrollY);
         };
-    
+
         window.addEventListener('scroll', handleScroll);
         return () => window.removeEventListener('scroll', handleScroll);
-      }, [lastScrollY]);
+    }, [lastScrollY]);
 
     const toggleDropdown = () => {
         setDropdownOpen(!dropdownOpen);
     };
 
+    const toggleSearch = () => {
+        setSearchOpen(!searchOpen);
+    };
 
     return (
         <div className={`navbar1 ${scrollingUp ? 'navbar-visible' : 'navbar-hidden'}`}>
             <div className="logo">
                 <Link to="/"><img src={logo} alt="Vía Óptima Dualab" className="logo-img" /></Link>
-
             </div>
             <nav className="nav-links1">
                 <Link to="/">Inicio</Link>
@@ -51,8 +55,20 @@ const NavbarWeb = () => {
                 <Link to="/recursos">Recursos</Link>
                 <Link to="/contact">Contáctenos</Link>
                 <Link to="/equipo">Equipo</Link>
-                <Link to="/perfil"><i class="bi bi-person-fill"></i></Link>
+                <Link to="/perfil"><i className="bi bi-person-fill"></i></Link>
+                <i className="bi bi-search" onClick={toggleSearch}></i>
             </nav>
+
+            {searchOpen && (
+                <div className="search-bar">
+                    <input
+                        type="text"
+                        placeholder="Busca en este sitio web"
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                    />
+                </div>
+            )}
         </div>
     );
 };
